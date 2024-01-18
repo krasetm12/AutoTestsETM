420978dc2b3bf9ae87d010b04170e3c5428689b7
Aleksei Krasikov <89021443+krasetm12@users.noreply.github.com>
2024-01-18 16:07:23 +0300

    update project124

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,49 +1,4 @@
-<<<<<<< HEAD
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
-import java.io.File;
-import java.net.URL;
-
-public class SimpleTest {
-    public RemoteWebDriver driver;
-    private static final String SELENIUM_URL = "http://localhost:4444/wd/hub";
-
-    @BeforeTest
-    public void start() throws Exception {
-        this.driver = new RemoteWebDriver(
-                new URL(SELENIUM_URL),
-                new ChromeOptions()
-        );
-    }
-    
-    @Test
-    public void simpleTest() throws Exception {
-        this.driver.get("https://google.com/");
-        this.takeSceenshot();
-    }
-
-    private void takeSceenshot() throws Exception {
-        TakesScreenshot ts = (TakesScreenshot)this.driver;
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(source, new File("./screenshot.png"));
-        System.out.println("The Screenshot is taken...");
-
-    }
-
-    @AfterTest
-    public void closeSeleniumSession() {
-        this.driver.close();
-        this.driver.quit();
-    }
-}
-=======
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -317,4 +272,4 @@ public class SimpleTest {
 
 
 }
->>>>>>> 699a7c5b4f4a6557ce8125f8d5a84a29d37ba12d
+
